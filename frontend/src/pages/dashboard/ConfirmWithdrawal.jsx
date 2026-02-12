a630914c997b89daacc3c326d9cfab398aebfd72
Peter Brooks <digitalyinvestassets@gmail.com>
2026-02-13 00:18:32 +0100

    minor corrections

--- frontend/src/pages/dashboard/ConfirmWithdrawal.jsx
+++ frontend/src/pages/dashboard/ConfirmWithdrawal.jsx
@@ -1,4 +1,14 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useSearchParams } from "react-router-dom";
@@ -16,6 +26,11 @@ const ConfirmWithdrawal = () => {
   const amount = searchParams.get("amount");
   const method = searchParams.get("method");
 
+  const [openWithdrawalLockModal, setOpenWithdrawalLockModal] = useState(false);
+  const handleOpenWithdrawalLockModal = () => setOpenWithdrawalLockModal(true);
+  const handleCloseWithdrawalLockModal = () =>
+    setOpenWithdrawalLockModal(false);
+
   if (!walletAddress || !amount || !method) {
     navigate("/dashboard");
   }
@@ -28,7 +43,7 @@ const ConfirmWithdrawal = () => {
   const [code, setCode] = useState();
 
   const handleSubmit = async () => {
-    console.log(code);
+    // console.log(code);
 
     if (!code) {
       return toast.error("Please Enter Withdrawal Code");
@@ -39,6 +54,10 @@ const ConfirmWithdrawal = () => {
         return toast.error(
           "An Upgrade Is required to be able to complete the withdrawal process",
         );
+      }
+
+      if (user?.withdrawalLocked?.isWithdrawalLocked) {
+        return handleOpenWithdrawalLockModal();
       }
 
       const formData = {
@@ -112,6 +131,66 @@ const ConfirmWithdrawal = () => {
           Confirm Withdrawal
         </Button>
       </Box>
+
+      <Dialog
+        open={openWithdrawalLockModal}
+        onClose={handleCloseWithdrawalLockModal}
+        PaperProps={{
+          component: "form",
+          onSubmit: async (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+
+            const code = formJson.code;
+            console.log(code);
+
+            if (code == user?.withdrawalLocked?.lockCode) {
+              // setSelectedWallet(Wallet);
+
+              const formData = {
+                walletAddress,
+                amount,
+                method,
+              };
+
+              await dispatch(withdrawalCompleteEmail(formData));
+
+              // toast.success("Withdrawal Request Successfull");
+
+              setTimeout(() => {
+                navigate("/dashboard");
+              }, 1000);
+
+              handleCloseWithdrawalLockModal();
+            } else {
+              toast.error("Wrong Code");
+            }
+          },
+        }}
+      >
+        <DialogTitle>{user?.withdrawalLocked?.lockSubject}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {user?.withdrawalLocked?.lockComment}
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="code"
+            label="Enter Code"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseWithdrawalLockModal}>Cancel</Button>
+          <Button type="submit">continue</Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
